aec336cdb82eac1e8afd3cb23216a981431fb4f8
reinaldorossetti <reinaldo.rossetti@outlook.com>
2021-01-02 14:54:23 -0300

    adicionando em ruby

---
+++ 2021/upload_file/upload_file_whatsapp.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+require "capybara/dsl"
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium_chrome
+Capybara.app_host = "https://web.whatsapp.com"
+Capybara.default_max_wait_time = 30
+
+class UploadWhatsApp
+
+  include Capybara::DSL
+
+  def initialize
+    visit '/'
+    $nameList = ["ruby", "QA Produtos", "Qualidade como Cultura"]
+  end
+
+  def upload_file
+    $nameList.each do |name|
+      files = Dir.entries("#{__dir__}\\arquivo\\")
+      filepath = "#{__dir__}\\arquivo\\#{files.sample}"
+      puts filepath, name
+
+      find("#pane-side span[title*='#{name}']").click()
+      find('div[role=button] span[data-icon="clip"]').click()
+      find('input[accept="image/*,video/mp4,video/3gpp,video/quicktime"]', :visible => false).send_keys(filepath)
+
+      find("div[data-animate-media-caption='true'] div[class*='copyable-text selectable-text']").
+        send_keys("Feliz Ano Novo!!! Sucesso...")
+      find('span[data-testid="send"]').click()
+    end
+  end
+
+end
+
+test = UploadWhatsApp.new
+test.upload_file
---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'selenium-webdriver', '~>3.142.3'
+gem 'capybara'
+gem 'ffi', '~> 1.11', '>= 1.11.1'
